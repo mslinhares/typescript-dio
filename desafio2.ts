de28fb19f2c6ee9ff2a10bc46836e35e26687e8f
mslinhares <mateussoaresl101@gmail.com>
2022-07-24 01:22:50 -0300

    commit inicial

---
+++ desafio2.ts
@@ -0,0 +1,62 @@
+// Como podemos melhorar o esse código usando TS? 
+
+/*let pessoa1 = {};
+pessoa1.nome = "maria";
+pessoa1.idade = 29;
+pessoa1.profissao = "atriz"
+
+let pessoa2 = {}
+pessoa2.nome = "roberto";
+pessoa2.idade = 19;
+pessoa2.profissao = "Padeiro";
+
+let pessoa3 = {
+    nome: "laura",
+    idade: "32",
+    profissao: "Atriz"
+};
+
+let pessoa4 = {
+    nome = "carlos",
+    idade = 19,
+    profissao = "padeiro"
+}>*/
+
+// Como podemos melhorar o esse código usando TS? 
+
+type Pessoa = {
+    nome: string,
+    idade: number,
+    profissao: Profissoes
+}
+
+enum Profissoes{
+    Atriz,
+    Padeiro
+}
+
+let pessoa1:Pessoa = {
+    nome: "maria",
+    idade: 29,
+    profissao: Profissoes.Atriz
+}
+
+let pessoa2 = {
+    nome: "roberto",
+    idade: 19,
+    profissao: Profissoes.Padeiro
+}
+
+let pessoa3 = {
+    nome: "laura",
+    idade: 32,
+    profissao: Profissoes.Atriz
+}
+
+let pessoa4 = {
+    nome: "carlos",
+    idade: 19,
+    profissao: Profissoes.Padeiro
+}
+
+console.log(pessoa1)
---
+++ desafio3.ts
@@ -0,0 +1,38 @@
+// O código abaixo tem alguns erros e não funciona como deveria.
+// Você pode identificar quais são e corrigi-los em um arquivo TS?
+
+let botaoAtualizar = document.getElementById('atualizar-saldo') as HTMLButtonElement;
+let botaoLimpar = document.getElementById('limpar-saldo')as HTMLButtonElement;
+let soma = document.getElementById('soma') as HTMLInputElement;
+let campoSaldo = document.getElementById('campo-saldo');
+
+let Saldo = 0
+limparSaldo()
+
+function limparCampoSoma() {
+    soma.value = "";
+}
+
+function somarAoSaldo(soma: number) {
+    if(campoSaldo){
+        Saldo += soma;   
+        campoSaldo.innerHTML = Saldo.toString()
+        limparCampoSoma()
+    }
+}
+
+function limparSaldo() {
+    if(campoSaldo){
+        Saldo = 0,
+        campoSaldo.innerHTML = Saldo.toString()
+        limparCampoSoma()
+    }
+}
+
+botaoAtualizar.addEventListener('click', function () {
+    somarAoSaldo(Number(soma.value));
+});
+
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
